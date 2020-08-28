7fc9901878b92c037e27d7bdf8f0debf8dc320a7
Esron Dtamar <esron.dtamar@gmail.com>
2020-08-27 21:14:34 -0300

    cria controller maratona

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MaratonaController } from './maratona/maratona.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, MaratonaController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/maratona/maratona.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('maratona')
+export class MaratonaController {}
