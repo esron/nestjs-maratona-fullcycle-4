e0a159b97f6a0779a6c8c09cef0ae4c9ffd36068
Esron Dtamar <esron.dtamar@gmail.com>
2020-08-27 22:31:21 -0300

    exporta categories module

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -8,5 +8,6 @@ import { Category } from './category.entity';
   imports: [TypeOrmModule.forFeature([Category])],
   providers: [CategoriesService],
   controllers: [CategoriesController],
+  exports: [TypeOrmModule, CategoriesService],
 })
 export class CategoriesModule {}
