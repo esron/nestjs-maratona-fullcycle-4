29e862daa2b59b7c9271bd74d76dc33b8d833500
Esron Dtamar <esron.dtamar@gmail.com>
2020-08-27 21:33:23 -0300

    configura o typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MaratonaController } from './maratona/maratona.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController, MaratonaController],
   providers: [AppService],
 })
